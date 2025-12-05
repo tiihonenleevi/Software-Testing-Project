4b46e5fb6d8baf28815122ac15b85006596b45df
tiihonenleevi <tiihonen.leevi13@gmail.com>
2025-12-05 08:10:47 +0200

    Fixed the test example so that lcov.info wouldn't be empty anymore

--- test/example.test.js
+++ test/example.test.js
@@ -1,7 +1,8 @@
 import { expect } from 'chai';
+import add from '../src/add.js';
 
 describe('Example Test', () => {
     it('should verify equality', () => {
-        expect(2 + 2).to.equal(4);
+        expect(add(2, 2)).to.equal(4);
     });
 });
